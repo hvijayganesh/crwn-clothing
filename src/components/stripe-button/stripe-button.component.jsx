9afa9ed3c4f2c2826ac4c035418cc96d509e25b3
Vijay Ganesh <vijay.ganesh@bhnetwork.com>
2021-01-07 10:21:24 +0530

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51I6puhEmeTvc6dibkY95an2mEQ3nvhVVKcrGYRw57WjPVn9CHq940sd2j3m6AIf7EDOECtFrMaaWmMhFl7N6OzmD00ShtwQ0N2';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amout={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton;
